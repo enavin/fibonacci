a49b5664256567e4a6b48ba6ba69b38c1c4758d7
enavin <enavin@gmail.com>
2013-06-06 04:44:25 -0700

    initial version

---
+++ src/test/java/com/sample/rest/controller/test/FibonacciControllerTest.java
@@ -0,0 +1,135 @@
+package com.sample.rest.controller.test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.UnsupportedEncodingException;
+
+import net.sf.json.JSONObject;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import com.sample.rest.controller.test.common.RestHelper;
+
+public class FibonacciControllerTest {
+
+
+		RestHelper restHelper = new RestHelper();
+		String serviceURL = "http://localhost:8080/sample/fibonacci";
+		
+		private Logger logger = LoggerFactory.getLogger(FibonacciControllerTest.class);
+		String xmlTestData = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><fibonacci><value index=\"0\">0</value><value index=\"1\">1</value><value index=\"2\">1</value><value index=\"3\">2</value><value index=\"4\">3</value></fibonacci>";
+		String jsonTestData = "{\"fibonacci\":{\"value\":[{\"index\":\"0\",\"text\":\"0\"},{\"index\":\"1\",\"text\":\"1\"},{\"index\":\"2\",\"text\":\"1\"},{\"index\":\"3\",\"text\":\"2\"},{\"index\":\"4\",\"text\":\"3\"}]}}";
+		
+		@Test
+		public void setup(){
+			// setup proxy or other perdefined data here...
+		}
+		
+		@Test
+		public void testGetFibonacciXML() {
+
+			logger.info("testGetFibonacci");
+
+			HttpHeaders requestHeaders = new HttpHeaders();
+			
+
+			MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+			parameters.add("seqNumber", "5");
+			
+			ResponseEntity<String> result;
+			try {
+				
+				logger.debug("URL :" + serviceURL);
+				
+				 result = restHelper.get(serviceURL , requestHeaders, parameters);
+				assertNotNull(result);
+				
+				assertEquals(HttpStatus.OK.value(),result.getStatusCode().value());
+				
+				assertNotNull(result.hasBody());
+				
+				String actualXML = result.getBody();
+				
+				assertEquals(xmlTestData.trim(),actualXML.trim());
+				
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+				fail();
+			}
+		}	
+		
+		@Test
+		public void testGetFibonacciXMLInvalidInput() {
+
+			logger.info("testGetFibonacciInvalidInput");
+
+			HttpHeaders requestHeaders = new HttpHeaders();
+			
+
+			MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+			parameters.add("seqNumber", "-1");
+			
+			ResponseEntity<String> result;
+			try {
+				
+				logger.debug("URL :" + serviceURL);
+				
+				result = restHelper.get(serviceURL , requestHeaders, parameters);
+				assertNotNull(result);
+				
+				assertEquals(HttpStatus.BAD_REQUEST.value(),result.getStatusCode().value());
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				assertNotNull(e.getMessage().indexOf(400) > 0);
+				
+			}
+		}	
+		
+		@Test
+		public void testGetFibonacciJSON() {
+
+			logger.info("testGetFibonacciJSON");
+
+			HttpHeaders requestHeaders = new HttpHeaders();
+			
+
+			MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+			parameters.add("seqNumber", "5");
+			
+			ResponseEntity<String> result;
+			try {
+				
+				logger.debug("URL :" + serviceURL);
+				
+				 result = restHelper.get(serviceURL + ".json", requestHeaders, parameters);
+				assertNotNull(result);
+				
+				assertEquals(HttpStatus.OK.value(),result.getStatusCode().value());
+				
+				assertNotNull(result.hasBody());
+				
+				JSONObject expectedObject = JSONObject.fromObject( jsonTestData );  
+				
+				JSONObject actualObject = JSONObject.fromObject( result.getBody() );  
+				
+				assertEquals(expectedObject,actualObject);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail();
+			}
+		}	
+		
+	
+}
---
+++ src/test/java/com/sample/rest/controller/test/common/RestHelper.java
@@ -0,0 +1,92 @@
+package com.sample.rest.controller.test.common;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * This helper class provides wrapper api for REST calls 
+ * 
+ * @author Naveen
+ *
+ */
+public class RestHelper {
+
+	RestTemplate restTemplate = new RestTemplate();
+	
+	public String post(String url, HttpHeaders requestHeaders, MultiValueMap<String, Object> parameters) throws UnsupportedEncodingException {
+		
+		encode(parameters);
+		
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(parameters, requestHeaders);
+		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+		return result.getBody();
+		
+	}
+	
+	public String put(String url, HttpHeaders requestHeaders, MultiValueMap<String, String> parameters) throws UnsupportedEncodingException {
+		
+		encodeParam(parameters);
+		
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(parameters, requestHeaders);
+		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+		return result.getBody();
+		
+	}
+	
+	public String delete(String url, HttpHeaders requestHeaders, MultiValueMap<String, String> parameters) throws UnsupportedEncodingException {
+		
+		encodeParam(parameters);
+		
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(parameters, requestHeaders);
+		ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, String.class);
+		return result.getBody();
+		
+	}
+	
+	public ResponseEntity<String> get(String url, HttpHeaders requestHeaders, MultiValueMap<String, String> parameters) throws UnsupportedEncodingException {
+		
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(parameters, requestHeaders);
+		String params = "";
+		if(parameters != null) {
+			params = null;
+			for (String key : parameters.keySet()) {
+				String value = parameters.get(key).get(0);
+				if (params == null) {
+					params = "?" + key + "=" + URLEncoder.encode(value, "utf-8");
+				} else {
+					params += "&" + key + "=" + URLEncoder.encode(value, "utf-8");
+				}
+			}
+		}
+		
+		ResponseEntity<String> result = restTemplate.exchange(url+(params==null?"":params), HttpMethod.GET, requestEntity, String.class);
+		return result;
+		
+	}
+	
+	private void encodeParam(MultiValueMap<String, String> postParameters) throws UnsupportedEncodingException {
+		for(String key : postParameters.keySet()) {
+			String value = postParameters.get(key).get(0);
+			postParameters.set(key, URLEncoder.encode(value, "utf-8"));
+		}
+	}
+	
+	private void encode(MultiValueMap<String, Object> postParameters) throws UnsupportedEncodingException {
+		for(String key : postParameters.keySet()) {
+			
+			if (postParameters.get(key).get(0) instanceof String) {
+				String value = (String) postParameters.get(key).get(0);
+				postParameters.set(key, URLEncoder.encode(value, "utf-8"));
+			}	
+			
+		}
+	}
+	
+}
